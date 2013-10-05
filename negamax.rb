84e5338b8e11ce0d44eb438baff539df27b13c4c
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-10-05 11:33:07 -0700

    Add #negamax method

--- negamax.rb
+++ negamax.rb
@@ -1,6 +1,16 @@
 module Negamax
-  def negamax(node, depth, alpha, beta, color)
+  def negamax(node, depth, alpha, beta, color_sign)
+    return color * node.value if depth == 0 || node.terminal?
+    best_value = -72.0
     
+    node.children.each do |child|
+      child_value = -negamax(child, depth - 1, -beta, -alpha, -color_sign)
+      best_value = max(best_value, child_value)
+      alpha = max(alpha, child_value)
+      break if alpha >= beta
+    end
+    
+    best_value
   end
 
   class MoveNode
